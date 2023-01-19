034699e1c1ad1d7ab238d7f20b20878fa939374d
Brittnie Sexton <brittniesexton95@gmail.com>
2023-01-19 09:29:48 -0600

    finished commenting the seed.cpp file

--- Source/FarmingModularSystem/Seed.cpp
+++ Source/FarmingModularSystem/Seed.cpp
@@ -14,35 +14,43 @@ ASeed::ASeed()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	// Create a collision sphere and set it as the root component
+	// Create a collision box and set it as the root component
 	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Collision Box"));
 	RootComponent = CollisionBox;
 
-	// Create a mesh that is attacehd to the collision sphere
+	// Create a mesh that is attacehd to the collision box
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
 	Mesh->SetupAttachment(RootComponent);
 
+	// Dynamic delegate that calls OnOverlapBegin in the ASeed class when the OnComponentBegineOverlap event is triggered
 	CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &ASeed::OnOverlapBegin);
 }
 
 void ASeed::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	// If the other actor is the player...
 	if (APawn* player = Cast<APawn>(OtherActor))
 	{
+		// ... set the player pawn to be a player character
 		playerCharacter = Cast<AFarmingModularSystemCharacter>(player);
 
+		// set a temp array to be the array on the player character
 		TArray<ASeed*> playerCharacterArray = playerCharacter->GetSeedArray();
 
+		// If the number of the array is at its max...
 		if (playerCharacterArray.Num() == playerCharacter->MaxArraySize)
 		{
-			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, TEXT("Array is full"));
+			//...exit the function
+			return;
 		}
+		// Otherwise...
 		else
 		{
+			//...add the seed the the array
 			playerCharacterArray.Add(this);
-			this->SeedData->PrintObjectInfo();
 		}
 
+		// Set the new array to be the array on the player character
 		playerCharacter->SetSeedArray(playerCharacterArray);
 	}
 }
